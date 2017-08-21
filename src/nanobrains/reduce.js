3bf61a11b95ccccb8ea4b151283af0529a4a7367
anton <anton.koch@gmail.com>
2017-08-21 12:42:59 +0200

    b0rked the reduce script...

--- src/nanobrains/reduce.js
+++ src/nanobrains/reduce.js
@@ -15,56 +15,57 @@ const processors = {},
   spinner = new CLI.Spinner(),
   ltt = new LMDBTransferTool(infile, outfile)
 
+const worker = function (args) {
+  let [id, ltt, proc, keyMillis, isRunning, resolve] = args
+  proc.stats.workTime = proc.seq.duration(function () {
+    if (spinner) spinner.message(`Sending... ${moment(Math.round(keyMillis)).format('HH:mm:ss:SSS')}`)
+
+    let keyDiff, entry = ltt.getCursorData(ltt.in.txn, id, true)
+    keyMillis = entry.key
+    keyDiff = entry.key.minus(keyMillis)
+    proc.frames.data = entry.data
+    while (isRunning && keyDiff.lt(proc.frames.interval.millis) && keyDiff.gte(Big(0))) {
+      if (!keyDiff.gte(0)) isRunning = false
+      proc.frames.interpolate(entry.data, PB.Frames.INTERPOLATE.MAX)
+      ltt.in.lmdb.advanceCursor(id)
+      entry = ltt.in.lmdb.getCursorData(ltt.in.txn, id, true)
+      keyDiff = entry.key.minus(keyMillis)
+    }
+
+    ltt.addRecord(proc.outId, keyMillis, proc.frames.data)
+  })
+
+  if (process.env.DEBUG) {
+    Logger.debug(`Work: ${proc.stats.workTime}μs`, 'cl:reduce')
+  }
+
+  if (isRunning) {
+    proc.seq.delay(0, worker, [id, ltt, proc, keyMillis, isRunning, resolve])
+  }
+  else {
+    ltt.close()
+    resolve()
+  }
+}
+
 Promise.map(ltt.in.lmdb.dbIds, function (id) {
+  const proc = {
+    frames: new PB.Frames(),
+    stats: new Stats(),
+    outId: undefined
+  }
+  ltt.in.lmdb.openDb(id)
+  ltt.in.lmdb.initCursor(ltt.in.txn, id)
+  proc.outId = ltt.out.lmdb.createDb(Object.assign({}, ltt.in.lmdb.meta[id]))
+  proc.frames.fps = process.env.FPS
+  processors[id] = proc
+
+  if (!process.env.DEBUG) spinner.start()
+
   return new Promise(function (resolve) {
-    let keyMillis, isRunning = true
-    const proc = {
-      frames: new PB.Frames(),
-      stats: new Stats(),
-      outId: undefined
-    }
-    ltt.in.lmdb.openDb(id)
-    proc.outId = ltt.out.lmdb.createDb(Object.assign({}, ltt.in.lmdb.meta[id]))
-    proc.frames.fps = process.env.FPS
-    processors[id] = proc
-
-    ltt.in.lmdb.initCursor(ltt.in.txn, id)
-    const worker = function (args) {
-      const [lmdb, proc] = args
-      proc.stats.workTime = proc.seq.duration(function () {
-        if (spinner) spinner.message(`Sending... ${moment(Math.round(keyMillis)).format('HH:mm:ss:SSS')}`)
-
-        let keyDiff, entry = lmdb.getCursorData(lmdb.in.txn, id, true)
-        keyMillis = entry.key
-        keyDiff = entry.key.minus(keyMillis)
-        proc.frames.data = entry.data
-        while (isRunning && keyDiff.lt(proc.frames.interval.millis) && keyDiff.gte(Big(0))) {
-          if (!keyDiff.gte(0)) isRunning = false
-          proc.frames.interpolate(entry.data, PB.Frames.INTERPOLATE.MAX)
-          lmdb.in.lmdb.advanceCursor(id)
-          entry = lmdb.in.lmdb.getCursorData(lmdb.in.txn, id, true)
-          keyDiff = entry.key.minus(keyMillis)
-        }
-
-        lmdb.addRecord(proc.outId, keyMillis, proc.frames.data)
-      })
-
-      if (process.env.DEBUG) {
-        Logger.debug(`Work: ${proc.stats.workTime}μs`, 'cl:reduce')
-      }
-
-      if (isRunning) {
-        proc.seq.delay(0, worker, [lmdb, proc])
-      }
-      else {
-        resolve()
-      }
-    }
-
-    if (!process.env.DEBUG) spinner.start()
+    proc.seq.delay(0, worker, [id, ltt, proc, null, true, resolve])
   })
 }).then(() => {
-  lmdb.close()
   process.exit(0)
 }).catch(err => {
   Logger.error(err.message)
