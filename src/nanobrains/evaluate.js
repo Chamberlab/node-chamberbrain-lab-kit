78bbc50f16ee10ac895b4cad757a4d4203b65195
anton <anton.koch@gmail.com>
2017-09-17 04:44:36 +0200

    fix data load/parse for log evaluation

--- src/nanobrains/evaluate.js
+++ src/nanobrains/evaluate.js
@@ -4,26 +4,35 @@ const path = require('path'),
 const basepath = process.env.BASE_PATH,
   filenames = fs.readdirSync(basepath)
 
-let entries = []
+const entries = {}
 filenames.forEach(filename => {
   if (path.extname(filename) === '.json') {
-    entries = entries.concat(JSON.parse(fs.readFileSync(path.join(basepath, filename))))
+    const log = JSON.parse(fs.readFileSync(path.join(basepath, filename)))
+    for (let id in log) {
+      if (!entries[id]) entries[id] = []
+      entries[id] = entries[id].concat(log[id].entries)
+    }
   }
 })
-entries.sort((a, b) => {
-  return a.args[0] > b.args[0] ? 1 : a.args[0] < b.args[0] ? -1 : 0
-})
-
-let lastMs, count = 0, frame = []
-for (let entry of entries) {
-  const ms = entry && Array.isArray(entry.args) ? entry.args[0] : undefined
-  if (ms) {
-    if (lastMs && ms !== lastMs) {
-      count++
-      if (count % 10000 === 0) console.log(`${count} frames processed`)
-      frame = []
+let count = 0
+for (let id in entries) {
+  entries[id].sort((a, b) => {
+    return a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0
+  })
+  let lastMs, frame = []
+  for (let entry of entries[id]) {
+    const ms = Array.isArray(entry) && entry.length ? entry[0] : undefined
+    if (ms) {
+      if (lastMs && ms !== lastMs) {
+        count++
+        if (count % 100000 === 0) console.log(`${count} entries processed`)
+        frame = []
+      }
+      frame.push(entry)
+      lastMs = ms
     }
-    frame.push(entry)
-    lastMs = ms
   }
 }
+
+console.log(`${count} entries processed`)
+console.log('Done.')
